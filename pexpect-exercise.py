7fddff51e5fcc85d441088ad0c70020088a5e0c7
NetAcad DEVASC <changeme@example.com>
2024-10-14 14:22:08 +0000

    No pass store

--- pexpect-exercise.py
+++ pexpect-exercise.py
@@ -1,14 +1,21 @@
 import pexpect
 import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv('../Python-Connect/venv/.env')
+username = os.getenv('TELNET_USER')
+passw = os.getenv('TELNET_PASSWORD')
+
 ip_add = ["172.31.114.3", "172.31.114.4"]
 
 for ip in ip_add:
     child = pexpect.spawn("telnet " + ip)
     child.expect('Username:')
-    child.sendline('admin')
+    child.sendline(username)
 
     child.expect('Password:')
-    child.sendline('cisco')
+    child.sendline(passw)
 
     if ip == "172.31.114.3":
         child.expect('#')
